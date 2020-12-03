75a67f9ffd45c25c70a6960358fbe416b9f3b0ee
Asep Gumelar <48475205+Asepgumelar@users.noreply.github.com>
2020-12-03 15:48:50 +0700

    edited

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,53 +1,42 @@
 @extends('layouts.global')
 
-@section('title') Create Category @endsection 
+@section('title') Create Category @endsection
 
 @section('content')
 
 <div class="col-md-8">
 
-  @if(session('status'))
-    <div class="alert alert-success">
-      {{session('status')}}
-    </div>
-  @endif 
+    @if(session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
 
-  <form 
-    enctype="multipart/form-data" 
-    class="bg-white shadow-sm p-3" 
-    action="{{route('categories.store')}}" 
-    method="POST">
+    <form enctype="multipart/form-data" class="bg-white shadow-sm p-3"
+        action="{{ route('categories.store') }}" method="POST">
+        @csrf
 
-    @csrf
+        <label>Category name</label><br>
+        <input type="text"
+            class="form-control {{ $errors->first('name') ? "is-invalid" : "" }}"
+            value="{{ old('name') }}" name="name">
+        <div class="invalid-feedback">
+            {{ $errors->first('name') }}
+        </div>
+        <br>
 
-    <label>Category name</label><br>
-    <input 
-      type="text" 
-      class="form-control {{$errors->first('name') ? "is-invalid" : ""}}"
-      value="{{old('name')}}"
-      name="name">
-    <div class="invalid-feedback">
-      {{$errors->first('name')}}
-    </div>
-    <br>
-    
-    <label>Category image</label>
-    <input 
-      type="file" 
-      class="form-control {{$errors->first('image') ? "is-invalid" : ""}}"
-      name="image">
-    <div class="invalid-feedback">
-      {{$errors->first('image')}}
-    </div>
-    
-    <br>
+        <label>Category image</label>
+        <input type="file"
+            class="form-control {{ $errors->first('image') ? "is-invalid" : "" }}"
+            name="image">
+        <div class="invalid-feedback">
+            {{ $errors->first('image') }}
+        </div>
 
-    <input 
-      type="submit" 
-      class="btn btn-primary" 
-      value="Save">
+        <br>
 
-  </form>
+        <input type="submit" class="btn btn-primary" value="Save">
+    </form>
 </div>
 
 @endsection
--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.global')
 
-@section('title') Detail Category @endsection 
+@section('title') Detail Category @endsection
 
 @section('content')
-  <div class="col-md-8">
+<div class="col-md-8">
     <div class="card">
-      <div class="card-body">
-        <label><b>Category name</b></label><br>
-        {{$category->name}}
-        <br><br>
+        <div class="card-body">
+            <label><b>Category name</b></label><br>
+            {{ $category->name }}
+            <br><br>
 
-        <label><b>Category slug</b></label><br>
-        {{$category->slug}}
-        <br><br>
+            <label><b>Category slug</b></label><br>
+            {{ $category->slug }}
+            <br><br>
 
-        <label><b>Category image</b></label><br>
-        @if($category->image)
-          <img src="{{asset('storage/' . $category->image)}}" width="120px">
-        @endif
-      </div>
+            <label><b>Category image</b></label><br>
+            @if($category->image)
+                <img src="{{ asset('storage/' . $category->image) }}" width="120px">
+            @endif
+        </div>
     </div>
-  </div>
-@endsection 
+</div>
+@endsection
